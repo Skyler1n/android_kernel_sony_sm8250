dba63f7480d2af25fe7480c070911b864b2214e1
Dedrimer <13420015738@sina.cn>
2022-09-20 20:00:33 +0800

    Revert "BACKPORT: fuse: Verify userspace asks to requeue interrupt that we really sent"
    This reverts commit b0c7b3017e3e1c9cb265c1a1ab5340d7d50b80c7.

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -486,15 +486,9 @@ put_request:
 	fuse_put_request(fc, req);
 }
 
-static int queue_interrupt(struct fuse_iqueue *fiq, struct fuse_req *req)
+static void queue_interrupt(struct fuse_iqueue *fiq, struct fuse_req *req)
 {
 	spin_lock(&fiq->lock);
-	/* Check for we've sent request to interrupt this req */
-	if (unlikely(!test_bit(FR_INTERRUPTED, &req->flags))) {
-		spin_unlock(&fiq->lock);
-		return -EINVAL;
-	}
-
 	if (list_empty(&req->intr_entry)) {
 		list_add_tail(&req->intr_entry, &fiq->interrupts);
 		/*
@@ -505,13 +499,12 @@ static int queue_interrupt(struct fuse_iqueue *fiq, struct fuse_req *req)
 		if (test_bit(FR_FINISHED, &req->flags)) {
 			list_del_init(&req->intr_entry);
 			spin_unlock(&fiq->lock);
-			return 0;
+			return;
 		}
 		wake_up(&fiq->waitq);
 		kill_fasync(&fiq->fasync, SIGIO, POLL_IN);
 	}
 	spin_unlock(&fiq->lock);
-	return 0;
 }
 
 static void request_wait_answer(struct fuse_conn *fc, struct fuse_req *req)
@@ -1988,7 +1981,7 @@ static ssize_t fuse_dev_do_write(struct fuse_dev *fud,
 		else if (oh.error == -ENOSYS)
 			fc->no_interrupt = 1;
 		else if (oh.error == -EAGAIN)
-			err = queue_interrupt(&fc->iq, req);
+			queue_interrupt(&fc->iq, req);
 
 		fuse_put_request(fc, req);
 
