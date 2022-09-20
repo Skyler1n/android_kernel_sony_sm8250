37234b286f98a2e136db8aa0a00cdffce4ef6bb1
Dedrimer <13420015738@sina.cn>
2022-09-20 20:00:44 +0800

    Revert "BACKPORT: fuse: don't need GETATTR after every READ"
    This reverts commit 3beb2c2fa500fdd5eafef8863dbdf789aca765fc.

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1002,9 +1002,7 @@ static int fuse_update_get_attr(struct inode *inode, struct file *file,
 
 int fuse_update_attributes(struct inode *inode, struct file *file)
 {
-	/* Do *not* need to get atime for internal purposes */
-	return fuse_update_get_attr(inode, file, NULL,
-				    STATX_BASIC_STATS & ~STATX_ATIME, 0);
+	return fuse_update_get_attr(inode, file, NULL, STATX_BASIC_STATS, 0);
 }
 
 int fuse_reverse_inval_entry(struct super_block *sb, u64 parent_nodeid,
