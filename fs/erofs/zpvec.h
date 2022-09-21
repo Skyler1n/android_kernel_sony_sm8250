8239b9c9f4d400cd714de8450b57e04943fd0b1d
Dedrimer <13420015738@sina.cn>
2022-09-21 19:38:59 +0800

    Revert "erofs: remove the occupied parameter from z_erofs_pagevec_enqueue()"
    This reverts commit 04599d22ccfe3ab7220670f0b554505d56357601.

--- fs/erofs/zpvec.h
+++ fs/erofs/zpvec.h
@@ -106,8 +106,10 @@ static inline void z_erofs_pagevec_ctor_init(struct z_erofs_pagevec_ctor *ctor,
 
 static inline bool z_erofs_pagevec_enqueue(struct z_erofs_pagevec_ctor *ctor,
 					   struct page *page,
-					   enum z_erofs_page_type type)
+					   enum z_erofs_page_type type,
+					   bool *occupied)
 {
+	*occupied = false;
 	if (!ctor->next && type)
 		if (ctor->index + 1 == ctor->nr)
 			return false;
